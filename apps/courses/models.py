6ae4b018eb1e5d89fe28e0f411f9a08047ff4438
shangzhuomac <wuyoulai0@163.com>
2023-01-29 22:41:19 +0800

    operation over

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.db import models
+from orgs.models import OrgInfo, TeacherIngo
 
 
 # Create your models here.
@@ -17,4 +20,54 @@ class CourseInfo(models.Model):
         ('qd', '前端开发'), ('hd', '后端开发')
     ), verbose_name="课程类别", max_length=5
     )
+    course_need = models.CharField(max_length=100, verbose_name="课程须知")
+    teacher_tell = models.CharField(max_length=100, verbose_name="老师教导")
+    orginfo = models.ForeignKey(OrgInfo, verbose_name="老师教导")
+    teacherinfo = models.ForeignKey(TeacherIngo, verbose_name="所属讲师")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
+    def __str__(self):
+        return self.name
+
+    class Meta
+        verbose_name = "课程信息"
+        verbose_name_plural = "课程信息"
+
+class LessonInfo(models.Model):
+    name = models.CharField(max_length=50, verbose_name="章节名称")
+    courseinfo = models.ForeignKey(CourseInfo, verbose_name="所属课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Meta
+        verbose_name = "章节信息"
+        verbose_name_plural = "章节信息"
+
+class VideoInfo(models.Model)
+    name = models.CharField(max_length=50, verbose_name="视频名称")
+    study_time = models.IntegerField(default=0, verbose_name="视频时长")
+    url = models.URLField(default='http://www.atguigu.com',verbose_name="视频链接",max_length=200)
+    lessoninfo = models.ForeignKey(LessonInfo,verbose_name="所属章节")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Meta
+        verbose_name = "视频信息"
+        verbose_name_plural = "视频信息"
+
+class SourceInfo(models.Model):
+    name = models.CharField(max_length=50,verbose_name="资源名称")
+    down_load = models.FileField(upload_to='source/',max_length=200,verbose_name="下载路径")
+    courseinfo = models.ForeignKey(CourseInfo,verbose_name="所属课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Meta
+        verbose_name = "资源信息"
+        verbose_name_plural = verbose_name
--- apps/operations/models.py
+++ apps/operations/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from datetime import datetime
+from users.models import UserProfile
+
 
 # Create your models here.
+class UserAsk(models.Model):
+    name = models.CharField(max_length=30, verbose_name="姓名")
+    phone = models.CharField(max_length=11, verbose_name="手机")
+    course = models.CharField(max_length=20, verbose_name="课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Mate:
+        verbose_name = '咨询信息'
+        verbose_name_plural = verbose_name
+
+
+class UserLove(models.Model):
+    love_man = models.ForeignKey(UserProfile, verbose_name="收藏用户")
+    love_id = models.IntegerField(verbose_name="收藏id")
+    love_type = models.IntegerField(choices=((1, 'org'),
+                                    (2, 'course'),
+                                    (3, 'teacher')),
+                                    verbose_name="收藏类别"
+                                )
+    love_state = models.BooleanField(default=False,verbose_name="收藏状态")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.love_man.username
+
+    class Mate:
+        verbose_name = '收藏信息'
+        verbose_name_plural = verbose_name
